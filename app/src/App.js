a0a3980131ee71b8028d829ef1054b2448b09075
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2020-12-31 20:42:12 -0500

    Switched To Google Charts

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Stock from './components/Stock';
-import TestChart from './components/testChart';
-
 function App() {
   return (
     <div className="App">
+      <Stock title='Apple' ticker='AAPL' />
       <Stock title='Microsoft' ticker='MSFT' />
-      <TestChart title='Apple Inc' ticker='AAPL'/>
+      <Stock title='Tesla' ticker='TSLA' />
+      
     </div>
   );
 }
--- app/src/components/Stock.js
+++ app/src/components/Stock.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import CanvasJSReact from '../canvasjs.react';
+import Chart from "react-google-charts";
+let yearInSecs = 31_536_000;
+let dateTo = Math.floor(Date.now() / 1000);
+let dateFrom = Math.floor(Date.now() / 1000) - yearInSecs;
 
-let CanvasJS = CanvasJSReact.CanvasJS;
-let CanvasJSChart = CanvasJSReact.CanvasJSChart;
-let dataPoints = [];
-let options;
+
 class Stock extends Component {
+
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       title: this.props.name,
       ticker: this.props.ticker,
@@ -17,36 +18,60 @@ class Stock extends Component {
       openID: 'stock-open-' + this.props.ticker,
       previousCloseID: 'stock-previous-close-' + this.props.ticker,
       dropDownID: 'stock-drop-down-' + this.props.ticker,
-      graphID: 'graph-container-' + this.props.ticker
-    };
+      graphID: 'graph-container-' + this.props.ticker,
+      chartData: [],
+      dataLoadingStatus: 'loading'
+    }
+  }
+  componentDidMount() {
+
+    let request = new XMLHttpRequest();
+    const price = document.getElementById(this.state.priceID);
+    const high = document.getElementById(this.state.highID);
+    const low = document.getElementById(this.state.lowID);
+    const open = document.getElementById(this.state.openID);
+    const previousClose = document.getElementById(this.state.previousCloseID);
+    let tempData = [['day', 'open', 'high', 'low', 'close']];
+    let loadState = 'loading';
+    let data;
+    request.open('GET', 'https://finnhub.io/api/v1/quote?symbol=' + this.state.ticker + '&token=buc39mf48v6oa2u4eqvg')
+    request.send();
+    request.onload = function () {
+      data = JSON.parse(this.response);
+      price.innerHTML = 'Price: ' + data.c;
+      high.innerHTML = 'High: ' + data.h;
+      low.innerHTML = 'Low: ' + data.l;
+      open.innerHTML = 'Open: ' + data.o;
+      previousClose.innerHTML = 'Close: ' + data.o;
+    }
+
+    request = new XMLHttpRequest();
+
+    request.open('GET', 'https://finnhub.io/api/v1/stock/candle?symbol=' + this.state.ticker + '&resolution=W&from=' + dateFrom + '&to=' + dateTo + '&token=buc39mf48v6oa2u4eqvg');
+    request.send();
+    request.onload = function () {
+      let data = JSON.parse(this.response);
+
+      console.log(data)
+      for (let i = 0; i < data.t.length; i++) {
+        let date = new Date(data.t[i] * 1000);
+        tempData.push([date, parseFloat(data.l[i]), parseFloat(data.o[i]), parseFloat(data.c[i]), parseFloat(data.h[i])]);
+      }
+      console.log(tempData)
+      loadState = 'ready';
+      setState();
+    }
+    let setState = () => {
+      this.setState({
+        dataLoadingStatus: loadState,
+        chartData: tempData
+      })
+    }
+
   }
   render() {
-    let options = {
-      theme: 'light2',
-      title: {
-        text: this.state.title,
-        fontFamily: "times new roman"
-      },
-      height: 400,
-      zoomEnabled: true,
-      axisY: {
-        includeZero: false,
-        prefix: "$ "
-      },
-      axisX: {
-        interval: 2,
-        intervalType: "month",
-        valueFormatString: "MMM-YY",
-        labelAngle: -45
-      },
-      data: [
-        {
-          type: "candlestick",
-          dataPoints: dataPoints
-        }
-      ]
-    }
-    return (
+    console.log('ready')
+    return this.state.dataLoadingStatus === 'ready' ? (
       <div className='stock'>
         <div className='stock-card'>
           <h2>{this.state.ticker}</h2>
@@ -60,51 +85,40 @@ class Stock extends Component {
         </div>
         <div className='graph-container'>
           <div className={this.state.graphID}>
-            <CanvasJSChart
-              options={options}
-              onRef={ref => this.chart = ref}
-              id={this.state.ticker}
+            <Chart
+              width={'100%'}
+              height={600}
+              chartType='CandlestickChart'
+              loader={<div>Loading Chart</div>}
+              data={this.state.chartData}
+              options={{
+                legend: 'none',
+                candlestick: {
+                  fallingColor: { strokeWidth: 0, fill: '#a52714' }, // red
+                  risingColor: { strokeWidth: 0, fill: '#0f9d58' }, // green
+                }
+              }}
             />
           </div>
         </div>
       </div>
-    );
-  }
-  componentDidMount() {
-    let request = new XMLHttpRequest();
-    let chart = this.chart;
-    let yearInSecs = 31_536_000 / 2;
-    let dateTo = Math.floor(Date.now() / 1000);
-    let dateFrom = Math.floor(Date.now() / 1000) - yearInSecs;
-    let price = document.getElementById(this.state.priceID);
-    let high = document.getElementById(this.state.highID);
-    let low = document.getElementById(this.state.lowID);
-    let open = document.getElementById(this.state.openID);
-    let previousClose = document.getElementById(this.state.previousCloseID);
-    request.open('GET', 'https://finnhub.io/api/v1/quote?symbol=' + this.state.ticker + '&token=buc39mf48v6oa2u4eqvg')
-    request.send();
-    request.onload = function () {
-      let data = JSON.parse(this.response);
-      price.innerHTML = 'Price: ' + data.c;
-      high.innerHTML = 'High: ' + data.h;
-      low.innerHTML = 'Low: ' + data.l;
-      open.innerHTML = 'Open: ' + data.o;
-      previousClose.innerHTML = 'Close: ' + data.o;
-    }
-    request = new XMLHttpRequest();
-    console.log(dataPoints)
-    request.open('GET', 'https://finnhub.io/api/v1/stock/candle?symbol=' + this.state.ticker + '&resolution=D&from=' + dateFrom + '&to=' + dateTo + '&token=buc39mf48v6oa2u4eqvg');
-    request.send();
-    request.onload = function () {
-      console.log(chart.data[0].dataPoints);
-      let data = JSON.parse(this.response);
-      for (let i = 0; i < data.t.length; i++) {
-        let date = new Date(data.t[i] * 1000);
-        dataPoints.push({ x: date, y: [data.o[i], data.h[i], data.l[i], data.c[i]] });
-      }
-      chart.render();
-    }
-  }
-}
+    ) : (
+        <div className='stock'>
+          <div className='stock-card'>
+            <h2>{this.state.ticker}</h2>
+            <ul className='stock-info-container'>
+              <li className='stock-info' id={this.state.priceID}>Price: </li>
+              <li className='stock-info' id={this.state.highID}>High:</li>
+              <li className='stock-info' id={this.state.lowID}>Low:</li>
+              <li className='stock-info' id={this.state.openID}>Open:</li>
+              <li className='stock-info' id={this.state.previousCloseID}>Previous Close:</li>
+            </ul>
+          </div>
+          <div>Fetching data from API</div>
+        </div>
 
+      )
+  }
+
+}
 export default Stock;
