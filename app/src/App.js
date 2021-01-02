d00a65ccb19fb51105e5ec466a163502d13f38ef
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2021-01-02 10:06:00 -0500

    Fully Functional
    Finished core functionality

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,7 @@ import Stock from './components/Stock';
 function App() {
   return (
     <div className="App">
+      <Stock title='First Solar' ticker='FSLR' />
       <Stock title='Apple' ticker='AAPL' />
       <Stock title='Microsoft' ticker='MSFT' />
       <Stock title='Tesla' ticker='TSLA' />
--- app/src/components/Stock.js
+++ app/src/components/Stock.js
@@ -3,6 +3,7 @@ import Chart from "react-google-charts";
 let yearInSecs = 31_536_000;
 let dateTo = Math.floor(Date.now() / 1000);
 let dateFrom = Math.floor(Date.now() / 1000) - yearInSecs;
+let resolution = 'W';
 
 
 class Stock extends Component {
@@ -10,7 +11,7 @@ class Stock extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      title: this.props.name,
+      title: this.props.title,
       ticker: this.props.ticker,
       priceID: 'stock-price-' + this.props.ticker,
       highID: 'stock-high-' + this.props.ticker,
@@ -24,7 +25,7 @@ class Stock extends Component {
     }
   }
   componentDidMount() {
-
+    console.log('mount')
     let request = new XMLHttpRequest();
     const price = document.getElementById(this.state.priceID);
     const high = document.getElementById(this.state.highID);
@@ -44,20 +45,26 @@ class Stock extends Component {
       open.innerHTML = 'Open: ' + data.o;
       previousClose.innerHTML = 'Close: ' + data.o;
     }
+    request = new XMLHttpRequest();
+    request.open('GET', 'https://finnhub.io/api/v1/stock/metric?symbol=' + this.state.ticker + '&metric=all&token=buc39mf48v6oa2u4eqvg');
+    request.send();
+    request.onload = function () {
+      data = JSON.parse(this.response);
+      console.log(data);
+    }
+
 
     request = new XMLHttpRequest();
 
-    request.open('GET', 'https://finnhub.io/api/v1/stock/candle?symbol=' + this.state.ticker + '&resolution=W&from=' + dateFrom + '&to=' + dateTo + '&token=buc39mf48v6oa2u4eqvg');
+    request.open('GET', 'https://finnhub.io/api/v1/stock/candle?symbol=' + this.state.ticker + '&resolution=' + resolution + '&from=' + dateFrom + '&to=' + dateTo + '&token=buc39mf48v6oa2u4eqvg');
     request.send();
     request.onload = function () {
       let data = JSON.parse(this.response);
-
       console.log(data)
       for (let i = 0; i < data.t.length; i++) {
         let date = new Date(data.t[i] * 1000);
         tempData.push([date, parseFloat(data.l[i]), parseFloat(data.o[i]), parseFloat(data.c[i]), parseFloat(data.h[i])]);
       }
-      console.log(tempData)
       loadState = 'ready';
       setState();
     }
@@ -69,8 +76,37 @@ class Stock extends Component {
     }
 
   }
+  changeTimeFrame(time) {
+    dateTo = Math.floor(Date.now() / 1000);
+    this.setState({
+      dataLoadingStatus: 'loading'
+    });
+    if(time === '1W') {
+      dateFrom = dateTo - 604_800;
+      resolution = '60'
+    }
+    if(time === '1M') {
+      dateFrom = dateTo - yearInSecs / 12;
+      resolution = 'D'
+    }
+    if(time === '6M') {
+      dateFrom = dateTo - yearInSecs / 2;
+      resolution = 'W'
+    }
+    if(time === '1Y') {
+      dateFrom = dateTo - yearInSecs;
+      resolution = 'W'
+    }
+    if(time === '5Y') {
+      dateFrom = dateTo - yearInSecs * 5;
+      resolution = 'M'
+    }
+    this.componentDidMount()
+    
+    console.log('changed time frame')
+  }
   render() {
-    console.log('ready')
+    console.log('render');
     return this.state.dataLoadingStatus === 'ready' ? (
       <div className='stock'>
         <div className='stock-card'>
@@ -84,10 +120,19 @@ class Stock extends Component {
           </ul>
         </div>
         <div className='graph-container'>
-          <div className={this.state.graphID}>
+          <div className='chart-container' id={this.state.graphID}>
+            <div className='timeframe-container'>
+              <ul className='timeframe-list'>
+                <li onClick={() => this.changeTimeFrame('1W')}>1W</li>
+                <li onClick={() => this.changeTimeFrame('1M')}>1M</li>
+                <li onClick={() => this.changeTimeFrame('6M')}>6M</li>
+                <li onClick={() => this.changeTimeFrame('1Y')}>1Y</li>
+                <li onClick={() => this.changeTimeFrame('5Y')}>5Y</li>
+              </ul>
+            </div>
             <Chart
               width={'100%'}
-              height={600}
+              height={800}
               chartType='CandlestickChart'
               loader={<div>Loading Chart</div>}
               data={this.state.chartData}
@@ -96,7 +141,8 @@ class Stock extends Component {
                 candlestick: {
                   fallingColor: { strokeWidth: 0, fill: '#a52714' }, // red
                   risingColor: { strokeWidth: 0, fill: '#0f9d58' }, // green
-                }
+                },
+                crosshair: { trigger: 'both' }
               }}
             />
           </div>
@@ -114,7 +160,9 @@ class Stock extends Component {
               <li className='stock-info' id={this.state.previousCloseID}>Previous Close:</li>
             </ul>
           </div>
-          <div>Fetching data from API</div>
+          <div className='graph-container'>
+            <div className='chart-container' id={this.state.graphID}>Fetching data from API</div>
+          </div>
         </div>
 
       )
